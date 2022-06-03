96940c3fd3ecbba74f8be5ab7281d8afeb11cd39
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-06-03 20:00:00 -0300

    create Agencia interface

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,8 @@
 package src;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+    private Agencia configurarServicos;
+    public Cliente(String nome, String sobrenome, String cpf, String sexo, String endereco, int telefone, String email) {
+        super(nome, sobrenome, cpf, sexo, endereco, telefone, email);
+    }
 }
