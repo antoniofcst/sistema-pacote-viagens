caff80380b11fa563c5f9716d9d397a69b17ab7d
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-06-04 15:32:59 -0300

    Created Cidade class

---
+++ src/com/sistemapacoteviagens/Cidade.java
@@ -0,0 +1,10 @@
+package src.com.sistemapacoteviagens;
+
+import java.util.ArrayList;
+
+public class Cidade {
+    private String nomeDaCidade;
+    private String EstadoDaCidade;
+    private String PaisDaCidade;
+    private ArrayList<String> atracoes = new ArrayList<>();
+}
