2c9ffe3cf54bd1cee83563bcd8731bb29f6482a2
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-06-04 15:08:58 -0300

    interface implements

--- src/com/sistemapacoteviagens/AgenciaInterface.java
+++ src/com/sistemapacoteviagens/AgenciaInterface.java
@@ -1,5 +1,9 @@
 package src.com.sistemapacoteviagens;
 
 public interface AgenciaInterface {
-
+    public void escolhaDeServico();
+    public void adicionarPacote();
+    public void alterarPacote();
+    public void excluirPacote();
+    public void consultarPacote();
 }
