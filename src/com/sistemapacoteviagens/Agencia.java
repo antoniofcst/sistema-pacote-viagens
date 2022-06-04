cc2af34a686ec53b170a6ed762b198ce53e66860
Half <henriqueantoniof@gmai.com>
2022-06-04 16:42:32 -0300

    Created new class agencia com metod Working in finaceiro class

--- src/com/sistemapacoteviagens/Agencia.java
+++ src/com/sistemapacoteviagens/Agencia.java
@@ -1,12 +1,16 @@
 package src.com.sistemapacoteviagens;
+import java.util.ArrayList;
 
 public class Agencia implements AgenciaInterface{
     private String nome_Agencia;
     private String cnpj_Agencia;
     private String local_Agencia;
     private String tipo_de_servico;
-    private String servico_da_agencia;
     private Financeiro financeiro;
     private PacotesDeViagens PacotesDeViagens;
     private Cliente cliente;
+
+    //ArrayList para adicionar os pacotes
+    ArrayList<PacotesDeViagens> Tipo_de_Servico = new ArrayList<>();
+        Tipo_de_Servico.add(PacotesDeViagens);
 }
