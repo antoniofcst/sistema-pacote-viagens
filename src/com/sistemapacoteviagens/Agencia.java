0b03b00bb618f2533ff77b063cd6a0725b402377
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-06-04 19:28:04 -0300

    changed Agencia

--- src/com/sistemapacoteviagens/Agencia.java
+++ src/com/sistemapacoteviagens/Agencia.java
@@ -2,15 +2,15 @@ package src.com.sistemapacoteviagens;
 import java.util.ArrayList;
 
 public class Agencia implements AgenciaInterface{
-    private String nome_Agencia;
-    private String cnpj_Agencia;
-    private String local_Agencia;
-    private String tipo_de_servico;
+    private String nomeAgencia;
+    private String cnpjAgencia;
+    private String localDaAgencia;
+    private ArrayList<PacotesDeViagens> tipoDeServicoOfertado = new ArrayList<>();
     private Financeiro financeiro;
     private PacotesDeViagens PacotesDeViagens;
     private Cliente cliente;
 
-    //ArrayList para adicionar os pacotes
-    ArrayList<PacotesDeViagens> Tipo_de_Servico = new ArrayList<>();
-        Tipo_de_Servico.add(PacotesDeViagens);
+
+
+
 }
