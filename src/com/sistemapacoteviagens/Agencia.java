07efafc5a031157ce5753d24ca5ea122c886e3b9
Half <henriqueantoniof@gmai.com>
2022-06-04 22:03:23 -0300

    Created new class agencia com metod Working in finaceiro class

--- src/com/sistemapacoteviagens/Agencia.java
+++ src/com/sistemapacoteviagens/Agencia.java
@@ -10,7 +10,28 @@ public class Agencia implements AgenciaInterface{
     private PacotesDeViagens PacotesDeViagens;
     private Cliente cliente;
 
+    public Agencia (String nma, String cnpj , String lda){
+        this.setNomeAgencia(nma);
+        this.setCnpjAgencia(cnpj);
+        this.setLocalDaAgencia(lda);
+    }
 
-
-
+    public String getNomeAgencia() {
+        return this.nomeAgencia;
+    }
+    public void setNomeAgencia(String nma) {
+        this.nomeAgencia = nma;
+    }
+    public String getCnpjAgencia() {
+        return this.cnpjAgencia;
+    }
+    public void setCnpjAgencia(String cnpj) {
+        this.cnpjAgencia = cnpj;
+    }
+    public String getLocalDaAgencia() {
+        return this.localDaAgencia;
+    }
+    public void setLocalDaAgencia(String lda) {
+        this.localDaAgencia = lda;
+    }
 }
