c45735292120422109e49eb87081a0a5a6f3f92f
Half <henriqueantoniof@gmai.com>
2022-06-04 21:44:27 -0300

    Created new class agencia com metod Working in finaceiro class

--- src/com/sistemapacoteviagens/Financeiro.java
+++ src/com/sistemapacoteviagens/Financeiro.java
@@ -1,6 +1,40 @@
 package src.com.sistemapacoteviagens;
 
+import com.sun.xml.internal.fastinfoset.algorithm.DoubleEncodingAlgorithm;
+
 public class Financeiro {
-    public String forma_de_pagamento();
+    private String FormaDePagamento;
+
+    public Financeiro (String fmp){
+        this.FormaDePagamento = fmp;
+    }
+    public String getFormaDePagamento() {
+        return this.FormaDePagamento;
+    }
+    public void setFormaDePagamento(String fp) {
+        this.FormaDePagamento = fp;
+    }
+
+     public void forma_de_pagamento(int opcao) {
+            switch (opcao) {
+            case 1: setFormaDePagamento("PIX"); break;
+            case 2: setFormaDePagamento("Cartão-Débito"); break;
+            case 3: setFormaDePagamento("Cartão-Crédito"); break;
+            case 4: setFormaDePagamento("Em espécie"); break;
+            default: System.out.println("Opção desconhecida, por favor selecionar uma opção válida!!!");
+        }
+    }
+    public void imprima_forma_de_pagamento(){
+        System.out.println("Escolha uma opção:");
+        System.out.println("Para pix digite: 1 ");
+        System.out.println("Para cartão-débito digite: 2");
+        System.out.println("Para cartão-crédito digite: 3");
+        System.out.println("Para dinheiro em espécie digite: 4");
+    }
+
+    public void menu(int opcao){
+        imprima_forma_de_pagamento();
+        forma_de_pagamento(opcao);
+    }
 
 }
