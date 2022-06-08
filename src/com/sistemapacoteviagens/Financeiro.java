8e306f91d879035a94b0ed56c62d40643674900c
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-06-08 09:08:50 -0300

    implementation choose payment method

--- src/com/sistemapacoteviagens/Financeiro.java
+++ src/com/sistemapacoteviagens/Financeiro.java
@@ -2,8 +2,29 @@ package src.com.sistemapacoteviagens;
 
 public class Financeiro {
     public String formaDePagamento;
-    private String choosePayment(String metodoDePagamento) {
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
 
+    private String choosePayment(String metodoDePagamento) {
+        switch (metodoDePagamento) {
+            case "PIX":
+                setFormaDePagamento(metodoDePagamento);
+                break;
+            case "Cartão":
+                setFormaDePagamento(metodoDePagamento);
+                break;
+            case "Boleto":
+                setFormaDePagamento(metodoDePagamento);
+                break;
+            case "Espécime":
+                setFormaDePagamento(metodoDePagamento);
+                break;
+            default:
+                setFormaDePagamento(null);
+                System.out.println("Nenhuma forma de pagamento cadastrada.");
+
+        }
     }
 
 }
