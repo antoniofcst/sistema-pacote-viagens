8ea83d9535d0b22c6467c8c27570b8c57e41a56c
Half <henriqueantoniof@gmai.com>
2022-06-10 16:12:58 -0300

    Created new class agencia com metod Working in finaceiro class

--- src/com/sistemapacoteviagens/Pacote_01.java
+++ src/com/sistemapacoteviagens/Pacote_01.java
@@ -2,11 +2,15 @@ package src.com.sistemapacoteviagens;
 
 public class Pacote_01 extends PacotesDeViagens{
     private double preco;
+    private String nome;
 
-    public Pacote_01 (double preco) {
+    public Pacote_01 (String nome, double preco) {
+        this.setNome(nome);
         this.setPreco(preco);
     }
 
     public double getPreco() { return this.preco; }
     public void setPreco(double p) { this.preco = p; }
+    public String getNome() { return this.nome; }
+    public void setNome(String nome) { this.nome = nome; }
 }
--- src/com/sistemapacoteviagens/PacotesDeViagens.java
+++ src/com/sistemapacoteviagens/PacotesDeViagens.java
@@ -1,7 +1,11 @@
 package src.com.sistemapacoteviagens;
 
+import java.util.ArrayList;
+
 public class PacotesDeViagens {
     public double precoDoPacote;
     public double getPrecoDoPacote() { return this.precoDoPacote; }
     public void setPrecoDoPacote(double precoDoPacote) { this.precoDoPacote = precoDoPacote; }
+
+    ArrayList<>
 }
